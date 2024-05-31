5cd13cdfb2c40ebe39083d304cc6b161f4685a87
Julia-KAERI <julia.kaeri@gmail.com>
2024-05-31 19:04:55 +0900

    20240531_1904

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,4 @@
-using Images, ColorSchemes
+using Images, ColorSchemes, CairoMakie
 
 """
     mat2gray(mat::Matrix{<:Real})
@@ -92,3 +92,39 @@ function _thist(img::Matrix{T}, dedge::S) where {T<:Real, S<:Real}
     return edges, counts
 end
 
+
+"""
+    image_profile(img, figsize, binsize)
+
+
+Generate makie figure with two plots. The left one is image and the right one is histogram of it
+"""
+function image_profile(img::Matrix; figsize::Union{Nothing, Tuple{Int64, Int64}}=nothing, binsize=2)
+    
+    if isa(figsize, Nothing) == false
+        @assert (400 < figsize[1] < 2100) && (400 <figsize[2] < 2000)
+    else         
+        figsize = (800, 400)
+    end
+
+    f = Figure(size = figsize)
+
+    Mv = maximum(img)
+    v1 = image(f[1, 1:2], img', axis=(aspect = DataAspect(), yreversed=true, xticklabelsvisible=false, yticklabelsvisible=false))
+    v2 = hist(f[1, 3], vec(img), bins = 0:binsize:Mv)
+
+    return f
+end
+
+"""
+vscode jupyter notebook 에서 각 함수, 객체 등에 관한 docstring 을 markdown 으로 출력한다.
+
+## Example
+
+@h sin
+"""
+macro h(x)
+    quote
+        display("text/markdown", @doc $x)
+    end    
+end
